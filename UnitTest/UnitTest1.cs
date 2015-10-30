b082d3d6894551bd62e8f385cf1245fc14d400c7
timoteid <timoteidig@yahoo.com>
2015-10-30 09:37:53 +0200

    UnitTests

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -1,15 +1,41 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ToDoApp;
+using System.Collections.Generic;
+using Should;
 
 namespace UnitTest
 {
+   
+
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestAddOneTask()
         {
+            List<string> testList = new List<string>();
+            string newLine = "new line";
+            ToDoTasks test = new ToDoTasks(testList);
+            test.AddTask(newLine);
+            testList = test.Get();
+            newLine.ShouldBeSameAs(testList[0]);
         }
+
+        [TestMethod]
+        public void TestIsEmptyMethod()
+        {
+            List<string> testList = new List<string>();
+            ToDoTasks test = new ToDoTasks(testList);
+            bool testIsEmpty = true;
+
+            testIsEmpty.ShouldEqual(test.IsEmpty());
+
+            string newLine = "new line";
+            test.AddTask(newLine);
+            testIsEmpty = false;
+            testIsEmpty.ShouldEqual(test.IsEmpty());
+        }
+
     }
 }
