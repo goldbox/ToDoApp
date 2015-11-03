c3a79cf543361777eccd022c31202485d8619ffe
timoteid <timoteidig@yahoo.com>
2015-11-03 09:30:03 +0200

    Fix the path bug.

--- ToDoApp/Program.cs
+++ ToDoApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ToDoApp
 {
@@ -10,23 +11,28 @@ namespace ToDoApp
     {
         static void Main(string[] args)
         {
-            TxtWorker txtWorker = new TxtWorker();
+            string path = Path.GetFullPath("ToDoAppTasksList.txt");
+            TxtWorker txtWorker = new TxtWorker(path);
             List<Task> initialList = txtWorker.Load();
             ToDoTasks tasksList = new ToDoTasks(initialList);
 
-            if (args.Length == 0 || (args[0] != "/add" && args[0] != "/list" && args[0] != "/done"))
+            if (IsNotAValidArgument(args))
             {
-                Console.WriteLine("Please enter one of the following arguments:");
-                Console.WriteLine("/add \"...\"- To add a new task");
-                Console.WriteLine("/done \"...\" - To set a particular task as done");
-                Console.WriteLine("/list - To see the list with opened tasks");
-                Console.WriteLine("/list done - To see the list with finished tasks");
-                Console.WriteLine("E.g. ToDoApp.exe add \"pay electricity\"");
+                Console.WriteLine("Use /? argument for help");
                 return;
             }
 
             switch (args[0])
             {
+                case "/?":
+                    Console.WriteLine("Please enter one of the following arguments:");
+                    Console.WriteLine("/add \"...\"- To add a new task");
+                    Console.WriteLine("/done taskID - To set a particular task as done");
+                    Console.WriteLine("/list - To see the list with opened tasks");
+                    Console.WriteLine("/list done - To see the list with finished tasks");
+                    Console.WriteLine("E.g. ToDoApp.exe add \"pay electricity\"");
+                    break;
+
                 case "/add":
                     if (args.Length >= 2)
                     {
@@ -35,16 +41,16 @@ namespace ToDoApp
                         Console.WriteLine("Task added successfully.");
                     }
                     break;
+
                 case "/list":
                     int index = 1;
-                    if(args.Length == 1)
+                    if (args.Length == 1)
                     {
                         if (tasksList.IsEmpty())
                         {
                             Console.WriteLine("No Tasks for today.\nEnjoy your day!");
                             break;
                         }
-                        index = 1;
                         Console.WriteLine("Here is the list with your opened tasks:");
                         foreach (Task task in tasksList)
                         {
@@ -53,49 +59,53 @@ namespace ToDoApp
                             index++;
                         }
                         break;
-                    }else
-                    {
-                    switch (args[1])
-                    {
-                        case "done":
-                            index = 1;
-                            Console.WriteLine("Here is the list with the finished tasks:");
-                            foreach (Task task in tasksList)
-                            {
-                                if (task.TaskStatus == false)
-                                    Console.WriteLine(index + ". " + task.Name);
-                                index++;
-                            }
-                            break;
-                        default:
-                            Console.WriteLine("/list - To see the list with opened tasks");
-                            Console.WriteLine("/list done - To see the list with finished tasks");
-                            break;
                     }
-                    }
+                    else
+                        switch (args[1])
+                        {
+                            case "done":
+                                Console.WriteLine("Here is the list with the finished tasks:");
+                                foreach (Task task in tasksList)
+                                {
+                                    if (task.TaskStatus == false)
+                                        Console.WriteLine(index + ". " + task.Name);
+                                    index++;
+                                }
+                                break;
+                            default:
+                                Console.WriteLine("/list - To see the list with opened tasks");
+                                Console.WriteLine("/list done - To see the list with finished tasks");
+                                break;
+                        }
                     break;
+
                 case "/done":
-                    if(args.Length >= 2)
+                    if (args.Length >= 2)
                     {
                         int i;
                         bool numberValid = Int32.TryParse(args[1], out i);
-                        if (numberValid && (i <= initialList.Count))
+                        if (numberValid && (i <= initialList.Count) && (i > 0))
                         {
-                            tasksList.ChangeTaskStatus(i-1, false);
+                            tasksList.ChangeTaskStatus(i - 1, false);
                             txtWorker.Save(tasksList);
-                            Console.WriteLine("Task " + i + " set to done!");
-                        } else   
+                            Console.WriteLine("Task " + i + " is set to done!");
+                        }
+                        else
                         {
                             Console.WriteLine("Please write the valid number of the finished task.");
                             Console.WriteLine("E.g. /done 1");
                         }
                     }
-
                     break;
+
                 default:
                     break;
             }
+        }
 
+        private static bool IsNotAValidArgument(string[] args)
+        {
+            return args.Length == 0 || (args[0] != "/add" && args[0] != "/list" && args[0] != "/done" && args[0] != "/?");
         }
     }
 }
