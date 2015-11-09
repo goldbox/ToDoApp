21852fabf9211be8f7e8917167229b573565b5f2
Timotei Dig <timoteidig@yahoo.com>
2015-11-09 08:31:42 +0200

    Partial HTML

--- ToDoApp/TxtWorker.cs
+++ ToDoApp/TxtWorker.cs
@@ -37,6 +37,37 @@ namespace ToDoApp
             sw.Close();
         }
 
+        public void SaveHtml (ToDoTasks toDoList)
+        {
+            string htmlPath = this.path;
+            htmlPath = htmlPath.Substring(0, (htmlPath.Length - 3));
+            htmlPath += "html";
+            StreamWriter sw = new StreamWriter(htmlPath, false);
+            sw.WriteLine("<!DOCTYPE html>");
+            sw.WriteLine("<html>");
+            sw.WriteLine("<head>");
+            sw.WriteLine("<title>To Do Tasks!</title>");
+            sw.WriteLine("</head>");
+            sw.WriteLine("<body>");
+            sw.WriteLine("<p>To Do Task List: </p>");
+            sw.WriteLine("<table>");
+            sw.WriteLine("<tr><td>Index</td><td>Is Open</td><td>Description</td></tr>");
+            toDoList.Reset();
+            int i = 0;
+            foreach (Task task in toDoList)
+            {
+                i++;
+                sw.WriteLine("<tr>");
+                sw.WriteLine("<td>{0}</td>", i);
+                sw.WriteLine("<td>{0}</td>", task.TaskStatus);
+                sw.WriteLine("<td>{0}</td>", task.Name.Replace('\n', '#'));
+                sw.WriteLine("</tr>");
+            }
+            sw.WriteLine("</table>");
+            sw.WriteLine("</body></html>");
+            sw.Close();
+        }
+
         public List<Task> Load()
         {
             List<Task> toDoList = new List<Task>();
@@ -45,7 +76,7 @@ namespace ToDoApp
             StreamReader sr = new StreamReader(this.path);
             while ((line = sr.ReadLine()) != null)
             {
-                line.Replace('#', '\n');
+                line = line.Replace('#', '\n');
                 string[] taskSettings = GiveMeTaskSettings(line);
                 bool openTask = (taskSettings[0] == "True");
                 toDoList.Add(new Task(openTask, taskSettings[1]));
